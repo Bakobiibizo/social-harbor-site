f839218f9dfe65c174d8664264e1f30e01d133da
bakobiibizo <bakobiibizo@gmail.com>
2026-07-11 14:33:23 -0700

    Add user and developer onboarding guides

--- tests/site.test.js
+++ tests/site.test.js
@@ -2,7 +2,7 @@ const test = require('node:test');
 const assert = require('node:assert/strict');
 const fs = require('node:fs');
 
-for (const page of ['index.html', 'add-friend/index.html', 'privacy/index.html']) {
+for (const page of ['index.html', 'add-friend/index.html', 'privacy/index.html', 'docs/index.html', 'docs/developers/index.html']) {
   test(`${page} has core document metadata`, () => {
     const html = fs.readFileSync(page, 'utf8');
     assert.match(html, /<html lang="en">/);
